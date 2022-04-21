5252191d7919d6fa4f5de29b7260221c5e58cd6b
Erik Moqvist <erik.moqvist@gmail.com>
2022-04-21 06:18:32 +0200

    Fix update script.

--- scripts/update_standard_library_build_results.py
+++ scripts/update_standard_library_build_results.py
@@ -1,10 +1,16 @@
+import os
+import glob
+import tempfile
+import shutil
 import subprocess
 import platform
+import tarfile
 import time
 from argparse import ArgumentParser
 import requests
 import html5lib
 from ansi2html import Ansi2HTMLConverter
+from xdg import xdg_cache_home
 
 
 def list_all_packages(url):
@@ -18,6 +24,10 @@ def list_all_packages(url):
         packages.append(row[0][0].text)
 
     return packages
+
+
+def clear_cache():
+    shutil.rmtree(xdg_cache_home() / 'mys/downloads', ignore_errors=True)
 
 
 def add_all_packages_to_dependencies(packages):
@@ -55,25 +65,41 @@ def create_log_header(package_root):
     return '\n'.join(header) + '\n\n'
 
 
-def build_package(package):
-    print(f'========================= {package} =========================')
-    package_root = f'stdall/build/dependencies/{package}-latest'
-    header = create_log_header(package_root)
-    command = ['mys', '-C', package_root, 'build']
-    proc = subprocess.run(command,
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT)
+def build_package(package, url):
+    with tempfile.TemporaryDirectory() as tempdir:
+        original_dir = os.getcwd()
+        os.chdir(tempdir)
 
-    if proc.returncode == 0:
-        result = 'yes'
-    else:
-        result = 'no'
+        try:
+            response = requests.get(f'{url}/package/{package}-latest.tar.gz')
+            response.raise_for_status()
 
-    log = header.encode('utf-8')
-    log += f'$ {" ".join(command)}\n'.encode('utf-8')
-    log += proc.stdout
+            with open('package-latest.tar.gz', 'wb') as fout:
+                fout.write(response.content)
 
-    return result, log
+            with tarfile.open('package-latest.tar.gz') as fin:
+                fin.extractall('package')
+
+            print(f'========================= {package} =========================')
+            package_root = glob.glob('package/*')[0]
+            command = ['mys', '-C', package_root, 'build', '--url', url]
+            proc = subprocess.run(command,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT)
+
+            if proc.returncode == 0:
+                result = 'yes'
+            else:
+                result = 'no'
+
+            header = create_log_header(package_root)
+            log = header.encode('utf-8')
+            log += f'$ {" ".join(command)}\n'.encode('utf-8')
+            log += proc.stdout
+
+            return result, log
+        finally:
+            os.chdir(original_dir)
 
 
 def create_html_log(log):
@@ -101,7 +127,7 @@ def upload_build_result_and_log(package, result, log, url):
 
 
 def build_and_upload_package(package, url):
-    result, log = build_package(package)
+    result, log = build_package(package, url)
     upload_build_result_and_log(package, result, log, url)
 
 
@@ -111,9 +137,7 @@ def main():
     args = parser.parse_args()
 
     packages = list_all_packages(args.url)
-    subprocess.run(['mys', 'new', 'stdall'], check=True)
-    add_all_packages_to_dependencies(packages)
-    subprocess.run(['mys', '-C', 'stdall', 'build', '-d', '--url', args.url], check=True)
+    clear_cache()
 
     for package in packages:
         build_and_upload_package(package, args.url)
